b3640a1b05520ec1ca0b9a1c41e40739b311be26
root <root@SGDNB0011.mut.kliniken.lan>
2020-02-29 22:25:57 +0100

    Create directory if not exist and copy files to backup directory

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Script to backup overgiven directory/file
 Require following arguments : 
 
 #Must write documentation
+
+Known Bugs :
+- single files cant be handled. Have to fix this
--- backupmydata.sh
+++ backupmydata.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 #
 #Perform daily backup of directory
+# backupmydata 
 date=$(date +%d_%m_%Y)
+#Set the value how long to keep files in days.
 olderthen=3
+#Set the destination backup directory
 backuppath=$(echo "/opt/backup/")
+#Set the source directory/file
 sourcepath=/etc/letsencrypt
 #Get the program / directory name for abstraction
 cd $sourcepath
 backupdirectory=$(basename $(pwd))
 #set debug for all commands
 debug=&>> /var/log/debug_backup_${backupdirectory}.log 
+#check if directorys exists, else create them first
+for directory in $backupdirectory ${backuppath}$date ${backuppath}${date}/${backupdirectory}
+do
+    if [ -d $directory ]
+    then
+        echo "$directory already exists. skip create"$debug
+    else
+        echo "failed to find $directory. Create it now! "$debug
+        mkdir $directory
+    fi 
+done
 cd $backuppath
-mkdir ${backuppath}${backupdirectory}_$date$debug
-cp -av ${sourcepath}/* ${backuppath}${backupdirectory}_${date} ${debug}
+#Check if file exists. Overwrite or create then.
+for file in $sourcepath/*; do 
+    if [ -f $file ]
+        then
+            echo "$file exists. Overwrite!"  $debug
+        else
+            echo "$file none exist. Create!"$debug
+    fi
+    cp -av $file ${backuppath}${date}/${backupdirectory} ${debug}
+done
 
 
 #Delete all Backups older than $olderthen days
 echo $olderthen
-find $backuppath${backupdirectory}_* -mtime +$olderthen -exec rm -rf {} \;$debug
+find $backuppath${date} -mtime +$olderthen -exec rm -rf {} \;$debug
