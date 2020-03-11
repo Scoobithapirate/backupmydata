2a6feeaa2313e0f878a1d3846a60249bbd53aea9
Tim Fuchs <tim.fuchs@outlook.de>
2020-03-11 18:35:22 +0100

    18:35:22

--- config.sh
+++ config.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
 #Perform daily backup of directory
 # backupmydata 
-date="date +%d_%m_%Y"
+date=$(date +%d_%m_%Y)
 #Set the value how long to keep files in days.
 olderthen=3
 #Set the destination backup directory
-backuppath="/opt/backup/"
+backuppath=$(echo "/opt/backup/")
 #Set log path
-logpath="/var/log/backupmydata"
+logpath=$(echo "/var/log/backupmydata")
 #Set the source directory/file
 sourcepath=/etc/letsencrypt
